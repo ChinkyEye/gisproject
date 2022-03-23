79a6826a16588a2f8a1160c434bc09e219dacc51
milan <khimdingmilan99@gmail.com>
2022-03-23 13:02:07 +0545

    some changes in backend

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,3 +26,21 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => 'ADMIN',
+        'email' => 'admin@techware.com.np',
+        'phone' => '9852012345',
+        'address' => 'Biratnagar',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$eAEF24Uto64jknFqlzRgXOZA7tWIiWNo3NB3dSpgkzQseTHOL7aIK', //admin123
+        'date_np' => date('Y-m-d'),
+        'date' => date('Y-m-d'),
+        'time' => date('H:i:s'),
+        'user_type' => '1',
+        'is_active' => '1',
+        'created_at' => date('Y-m-d H:i:s'),
+    ];
+});
+
