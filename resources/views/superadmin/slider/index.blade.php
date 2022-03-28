30758a1de63af2a1b18ba29a3338f230dbc17597
birsardar <birsardar@gmail.com>
2022-03-28 12:46:14 +0545

    slider done

--- resources/views/superadmin/slider/index.blade.php
+++ resources/views/superadmin/slider/index.blade.php
@@ -15,7 +15,7 @@
     </div><!-- /.container-fluid -->
   </div>
   <!-- /.content-header -->
-  <!-- Main content -->
+  <!-- Main content --> 
   <section class="content">
     <div class="container-fluid">
       <div class="row">
