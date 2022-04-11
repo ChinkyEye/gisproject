6aaee2bb188f3c9f7c83775564fdb30af03bbe4f
Sumit <git>
2022-04-11 16:16:12 +0545

    fixes

--- resources/views/web/layouts/left-menu.blade.php
+++ resources/views/web/layouts/left-menu.blade.php
@@ -1,48 +1,15 @@
 <div class="items">
-    <div class="items-body">
-      
-      @foreach($left_menu as $key => $data)
-        @if($data->link_type == 0)
-        <a href="{{ route('web.home.sidelink',$data->link) }}" class="items-body-content">
-          <span>{{$data->name}}</span>
-        </a>
-        @else
-        <a href="{{ $data->link }}" class="items-body-content" target="_blank">
-          <span>{{$data->name}}</span>
-        </a>
-        @endif
-      @endforeach
-      {{-- <a class="items-body-content">
-        <span>
-            मुख्यमन्त्री तथा मन्त्रिपरिषद्को कार्यालय
-        </span>
-      </a>
-      <div class="items-body-content">
-        <span>आर्थिक मामिला तथा योजना मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>सडक पूर्वाधार तथा शहरी विकास मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>आन्तरिक मामिला तथा कानून मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>स्वास्थ्य मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>खानेपानी, सिंचाइ तथा उर्जा मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>सामाजिक विकास मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>सामाजिक विकास मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>कृषि मन्त्रालय</span>
-      </div>
-      <div class="items-body-content">
-        <span>पर्यटन तथा संस्कृति मन्त्रालय</span>
-      </div> --}}
-    </div>
+  <div class="items-body">
+    @foreach($left_menu as $key => $data)
+    @if($data->link_type == 0)
+    <a href="{{ route('web.home.sidelink',$data->link) }}" class="items-body-content">
+      <span>{{$data->name}}</span>
+    </a>
+    @else
+    <a href="{{ $data->link }}" class="items-body-content" target="_blank">
+      <span>{{$data->name}}</span>
+    </a>
+    @endif
+    @endforeach
   </div>
+</div>
