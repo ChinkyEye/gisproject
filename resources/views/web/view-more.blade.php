fad815db47716adf1bccc062304405d565b01a43
Sumit <git>
2022-04-15 12:57:57 +0545

    view more

--- resources/views/web/view-more.blade.php
+++ resources/views/web/view-more.blade.php
@@ -3,46 +3,37 @@
 About | 
 @endpush
 @section('content')
-
+<nav class="bg-light breadcrumb-main">
+	<div class="container">
+		<ol class="breadcrumb bg-light align-content-center">
+			<li class="breadcrumb-item my-auto">
+				<a href="{{ route('web.home') }}">
+					<i class="fa fa-home fa-2x"></i>
+				</a>
+			</li>
+			<li class="breadcrumb-item my-auto">
+				<a href="#" class="">Library</a>
+			</li>
+			<li class="breadcrumb-item my-auto active">
+				<a href="javascript:void(0);" class="font-weight-normal">
+					Data
+				</a>
+			</li>
+		</ol>
+	</div>
+</nav>
 <section class="breadcrumb-main my-4">
 	<div class="container">
+		<div class="section-heading ">
+		    <h2>Title here</h2>
+		    <div class="line"></div>
+		</div>
 		<div class="row">
+			<div class="col-md-12{{--  text-justify --}}">
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam natus voluptates nulla amet tempora aliquid omnis error obcaecati officia doloremque debitis incidunt, quae corporis dolore, voluptatum reiciendis necessitatibus repellendus. Debitis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam natus voluptates nulla amet tempora aliquid omnis error obcaecati officia doloremque debitis incidunt, quae corporis dolore, voluptatum reiciendis necessitatibus repellendus. Debitis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam natus voluptates nulla amet tempora aliquid omnis error obcaecati officia doloremque debitis incidunt, quae corporis dolore, voluptatum reiciendis necessitatibus repellendus. Debitis?</p>
+			</div>
 			<div class="col-12">
-				<div class="table-responsive">
-					<table class="table table-bordered table-hover table-striped">
-						<thead class="text-center thead-dark">
-							<tr>
-								<th width="5%">SN</th>
-								<th class="text-left">Title</th>
-								<th width="10%">Date</th>
-								<th width="10%">Created At</th>
-								<th width="10%">File</th>
-								<th width="10%">Read More</th>
-							</tr>
-						</thead>
-						<tbody class="text-center">
-							@foreach($datas as $data)
-							<tr>
-								<td>{{$loop->iteration}}</td>
-								<td class="text-left">{{$data->title}}</td>
-								<td>{{$data->date_np}}</td>
-								<td><span class="badge badge-info">{{$data->created_at->diffForHumans()}}</span></td>
-								<td>
-									<a href="{{URL::to('/')}}/{{$data->path}}/{{$data->document}}" target="_blank" class="text-danger text-center">
-				                          <i class="fa fa-file-pdf-o fa-2x"></i>
-				                    </a>
-								</td>
-								<td>
-									<a class="effect1" href="{{ route('web.home.detail', [$link, $data->id ]) }}">
-                              {{ __('language.read_more')}}
-                              <span class="bg"></span>
-                            </a>
-								</td>
-							</tr>
-							@endforeach
-						</tbody>
-					</table>
-				</div>
+				<a href="" class="btn bg-main-blue"><i class="fa fa-download"></i> Download</a>
 			</div>
 		</div>
 	</div>
