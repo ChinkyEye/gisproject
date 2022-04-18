9f11e93ecd0d9ffb530538fc4c1afada98525285
milan <khimdingmilan99@gmail.com>
2022-04-18 18:04:16 +0545

    s

--- public/js/app.js
+++ public/js/app.js
@@ -40543,13 +40543,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! F:\xampp\htdocs\gisproject\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\xampp\htdocs\gisproject\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! D:\xamlaravel\htdocs\gisproject\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xamlaravel\htdocs\gisproject\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> origin/master
+__webpack_require__(/*! C:\xampp\htdocs\gisproject\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\gisproject\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
