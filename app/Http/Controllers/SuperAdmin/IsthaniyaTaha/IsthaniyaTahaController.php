08f900c7b136f2a50c06b5d923182715a7a58671
birsardar <birsardar@gmail.com>
2022-05-02 16:01:36 +0545

    istaniyataha added

--- app/Http/Controllers/SuperAdmin/IsthaniyaTaha/IsthaniyaTahaController.php
+++ app/Http/Controllers/SuperAdmin/IsthaniyaTaha/IsthaniyaTahaController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\SuperAdmin\IsthaniyaTaha;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\IsthaniyaTaha;
+use Auth;
+use Response;
+use File;
 
 class IsthaniyaTahaController extends Controller
 {
@@ -14,7 +18,8 @@ class IsthaniyaTahaController extends Controller
      */
     public function index()
     {
-        //
+        $datas = IsthaniyaTaha::get();
+       return view('superadmin.istaniyataha.index', compact('datas'));
     }
 
     /**
@@ -24,7 +29,7 @@ class IsthaniyaTahaController extends Controller
      */
     public function create()
     {
-        //
+        return view('superadmin.istaniyataha.create');
     }
 
     /**
@@ -35,7 +40,32 @@ class IsthaniyaTahaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $this->validate($request, [
+            
+          
+        ]);
+         $datas = IsthaniyaTaha::create([
+            'metropolitan' => $request['metropolitan'],
+            'sub_metropolitan' => $request['sub_metropolitan'],
+            'municipalities' => $request['municipalities'],
+            'sub_metropolitan' => $request['sub_metropolitan'],
+            'rural_municipalities' => $request['rural_municipalities'],
+            'forest_area' => $request['forest_area'],
+            'population' => $request['population'],
+            'agricultural_land' => $request['agricultural_land'],
+            'tourists_site' => $request['tourists_site'],
+            'electricity_dev' => $request['electricity_dev'],
+            'district' => $request['district'],
+            'wada' => $request['wada'],
+            'industries' => $request['industries'],
+            'is_active' => '1',
+            'date_np' => $this->helper->date_np_con_parm(date("Y-m-d")),
+            'date' => date("Y-m-d"),
+            'time' => date("H:i:s"),
+            'created_by' => Auth::user()->id,
+        ]);
+      
+        return redirect()->route('superadmin.istaniyataha.index')->with('alert-success', 'Link added successfully');;
     }
 
     /**
@@ -57,7 +87,8 @@ class IsthaniyaTahaController extends Controller
      */
     public function edit($id)
     {
-        //
+        $datas = IsthaniyaTaha::find($id);
+        return view('superadmin.istaniyataha.edit', compact('datas'));
     }
 
     /**
@@ -69,7 +100,17 @@ class IsthaniyaTahaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       $this->validate($request, [
+            
+        ]);
+        $datas = IsthaniyaTaha::find($id);
+        $all_data = $request->all();
+        $all_data['updated_by'] = Auth::user()->id;
+        if($datas->update($all_data))
+        {
+            return redirect()->route('superadmin.istaniyataha.index')->with('alert-success', 'Data updated succesffully!!!!');;
+        };
+
     }
 
     /**
@@ -80,6 +121,36 @@ class IsthaniyaTahaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $datas = IsthaniyaTaha::find($id);
+        $datas->delete();
+        return response()->json([
+            'success' => 'Record has been deleted successfully!'
+        ]);
+    }
+    public function isActive(Request $request,$id)
+    {
+        $get_is_active = IsthaniyaTaha::where('id',$id)->value('is_active');
+        $isactive = IsthaniyaTaha::find($id);
+        if($get_is_active == 0){
+        $isactive->is_active = 1;
+        $notification = array(
+          'message' => $isactive->name.' is Active!',
+          'alert-type' => 'success'
+        );
+        }
+        else {
+        $isactive->is_active = 0;
+        $notification = array(
+          'message' => $isactive->name.' is inactive!',
+          'alert-type' => 'error'
+        );
+        }
+        if(!($isactive->update())){
+        $notification = array(
+          'message' => $isactive->name.' could not be changed!',
+          'alert-type' => 'error'
+        );
+        }
+        return back()->with($notification)->withInput();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -164,6 +164,8 @@ Route::namespace('SuperAdmin')->prefix('home')->name('superadmin.')->middleware(
      Route::resource('sidebar-part/pradeshsarkar','PradeshSarkar\PradeshSarkarController');
      Route::get('sidebar-part/pradeshsarkar/active/{id}', 'PradeshSarkar\PradeshSarkarController@isActive')->name('pradeshsarkar.active');
 
+    Route::resource('/istaniyataha','IsthaniyaTaha\IsthaniyaTahaController');
+     Route::get('/istaniyataha/active/{id}', 'IsthaniyaTaha\IsthaniyaTahaController@isActive')->name('istaniyataha.active');
 });
 
 
@@ -195,7 +197,7 @@ Route::namespace('User')->prefix('user')->name('user.')->middleware(['user'])->g
     Route::get('surveyform/surveyanswer/{id}', 'SurveyFormController@getSurveyanswer')->name('surveyform.surveyanswer');
     Route::get('/survey-question','SurveyFormController@survey_question')->name('survey-question');
 
-    Route::resource('/istaniyataha','IstaniyaTaha\IstaniyaTahaController');
+
 
 });
 
