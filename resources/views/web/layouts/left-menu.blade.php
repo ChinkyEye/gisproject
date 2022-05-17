94a0770ce8458eac2c022eb4839efebef3f797e1
milan <khimdingmilan99@gmail.com>
2022-05-17 12:36:41 +0545

    icone in left menu

--- resources/views/web/layouts/left-menu.blade.php
+++ resources/views/web/layouts/left-menu.blade.php
@@ -4,7 +4,17 @@
     @if($data->link_type == 0)
     <a href="{{ route('web.home.sidelink',$data->link) }}" class="items-body-content">
       <span>
+        @if($key == 0)
         <i class="fa fa-briefcase mr-2"></i>
+        @elseif($key == 6)
+        <i class="fa fa-cog mr-2"></i> 
+        @elseif($key == 7)
+        <i class="fa fa-user mr-2"></i>
+        @elseif($key == 9)
+        <i class="fa fa-globe mr-2"></i>
+        @else
+        <i class="fa fa-briefcase mr-2"></i>
+        @endif
         {{-- आयोग --}}
         @if(app()->getLocale() == 'en')
         {{$data->name}}
@@ -22,7 +32,12 @@
     @else
     <a href="{{ $data->link }}" class="items-body-content" target="_blank">
       <span>
-        <i class="fa fa-briefcase mr-2"></i>
+        @if($key == 8)
+        <i class="fa fa-map-marker mr-2"></i>
+        @elseif($key == 5)
+        <i class="fa fa-university mr-2"></i>
+        @endif
+        {{-- <i class="fa fa-briefcase mr-2"></i> --}}
         @if(app()->getLocale() == 'en')
         {{$data->name}}
         @else
